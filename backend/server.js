061106390ec17f3945563fb9f742633d7c117a67
TimManas <timothy.romero.manas@gmail.com>
2021-01-23 00:02:29 -0500

    Added listening ports'

--- backend/server.js
+++ backend/server.js
@@ -50,4 +50,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Set up backend to listen to PORT 5000
-app.listen(PORT, console.log('Server is running on PORT 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .inverse
+  )
+)
