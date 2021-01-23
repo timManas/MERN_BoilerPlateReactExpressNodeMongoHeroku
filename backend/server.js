3c28d667f216bf38be92e945a6506c8d20608626
TimManas <timothy.romero.manas@gmail.com>
2021-01-22 22:31:58 -0500

    First commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+const PORT = 5000
+
+// Initialize Dot Env File
+dotenv.config()
+
+// Initialize Express
+const app = express()
+
+// Initialize MongoDB
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(`Error: ${error.message}`)
+    process.exit(1) // exit(1) - means you return with error
+  }
+}
+
+// Connect Backend to MongoDB
+connectDB()
+
+// Set up backend to listen to PORT 5000
+app.listen(PORT, console.log('Server is running on PORT 5000'))
