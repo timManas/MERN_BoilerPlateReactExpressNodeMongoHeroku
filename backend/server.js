97773ecf45c077823c945914ccb022146ee21607
TimManas <timothy.romero.manas@gmail.com>
2021-01-22 23:57:57 -0500

    Added Produciton Route

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,25 @@ const connectDB = async () => {
 connectDB()
 
 // Initialize the Routes
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
+// app.get('/', (req, res) => {
+//   res.send('API is running')
+// })
+
+// RUN THIS ONLY IN PRODUCTION MODE
+if (process.env.NODE_ENV === 'production') {
+  // Set the 'build' folder as a static folder
+  // Why ? So we can access the build folder and load the index.html
+  app.use(express.static(path.join(__dirname, '/frontend/build'))) // set frontend folder as a static folder
+
+  // Send the index.html file if on Production
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ....')
+  })
+}
 
 // Set up backend to listen to PORT 5000
 app.listen(PORT, console.log('Server is running on PORT 5000'))
